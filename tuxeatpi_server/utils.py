16339d197e09e250c3c3a21af73e662fa960d0f2
Thibault Cohen <titilambert@ttb.lt>
2019-04-08 12:31:51 -0400

    Backup for resintall

--- tuxeatpi_server/utils.py
+++ tuxeatpi_server/utils.py
@@ -12,3 +12,7 @@ def get_main_folder():
 def cors_support(response, *args, **kwargs):  # pylint: disable=W0613
     """Add cors support for an endpoint"""
     response.set_header('Access-Control-Allow-Origin', '*')
+    response.set_header('Access-Control-Allow-Methods', 'POST, GET')
+    response.set_header('Access-Control-Allow-Headers', 'Content-Type')
+
+
